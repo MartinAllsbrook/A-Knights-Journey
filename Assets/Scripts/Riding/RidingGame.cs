f1152ad199d558c3bee9f8bed2e2e7da9a696c49
MartinAllsbrook <martin.allsbrook@colorado.edu>
2026-02-19 15:21:58 -0500

    Moving world in Riding Game

--- Assets/Scripts/Riding/RidingGame.cs
+++ Assets/Scripts/Riding/RidingGame.cs
@@ -7,13 +7,19 @@ class RidingGame : MonoBehaviour
 
     [Header("References")]
     [SerializeField] GameObject gameOverScreen;
-
+    [SerializeField] Rigidbody2D chunksParent;
     [Header("Prefabs")]
     [SerializeField] Transform[] chunkPrefabs;
 
     [Header("Chunk Settings")]
     [SerializeField] int chunkSize = 50;
     [SerializeField] int loadedDistance = 3;
+
+    [Header("Speed Settings")]
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float moveSpeedIncreaseRate = 0.05f;
+
+    public float MoveSpeed => moveSpeed;
 
     readonly Dictionary<int, Transform> loadedChunks = new Dictionary<int, Transform>();
 
@@ -30,8 +36,16 @@ class RidingGame : MonoBehaviour
         if (RidingPlayer.Instance == null)
             return;
 
-        int currentChunk = Mathf.FloorToInt(RidingPlayer.Instance.transform.position.y / chunkSize);
+        moveSpeed += moveSpeedIncreaseRate * Time.deltaTime;
+
+        float playerY = RidingPlayer.Instance.transform.position.y;
+        int currentChunk = Mathf.FloorToInt((playerY - chunksParent.position.y) / chunkSize);
         UpdateLoadedChunks(currentChunk);
+    }
+
+    void FixedUpdate()
+    {
+        chunksParent.linearVelocity = Vector2.down * moveSpeed;
     }
 
     void UpdateLoadedChunks(int currentChunk)
@@ -64,8 +78,9 @@ class RidingGame : MonoBehaviour
     Transform SpawnChunk(int chunkIndex)
     {
         int randomIndex = Random.Range(0, chunkPrefabs.Length);
-        int yPosition = chunkIndex * chunkSize;
-        return Instantiate(chunkPrefabs[randomIndex], new Vector3(0, yPosition, 0), Quaternion.identity);
+        Transform chunk = Instantiate(chunkPrefabs[randomIndex], chunksParent.transform);
+        chunk.localPosition = new Vector3(0, chunkIndex * chunkSize, 0);
+        return chunk;
     }
 
     public void PlayerHit()
--- Assets/Scripts/Riding/RidingPlayer.cs
+++ Assets/Scripts/Riding/RidingPlayer.cs
@@ -4,24 +4,11 @@ public class RidingPlayer : MonoBehaviour
 {
     public static RidingPlayer Instance { get; private set; }
 
-    [Header("Riding Settings")]
-    [SerializeField] float moveSpeed = 5f;
-    [SerializeField] float moveSpeedIncreaseRate = 0.05f;
-
-    public float MoveSpeed => moveSpeed;
-
     void Awake()
     {
         if (Instance == null)
             Instance = this;
         else
             Destroy(gameObject);
-    }
-
-    void Update()
-    {
-        transform.Translate(Vector2.up * moveSpeed * Time.deltaTime);
-
-        moveSpeed += moveSpeedIncreaseRate * Time.deltaTime;
     }
 }
