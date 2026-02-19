036ef159f01cd7a67394771058313ba8d4d64219
MartinAllsbrook <martin.allsbrook@colorado.edu>
2026-02-19 14:55:52 -0500

    Updates to horse game

--- Assets/Scripts/Riding/Rider.cs
+++ Assets/Scripts/Riding/Rider.cs
@@ -4,11 +4,60 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class Rider : MonoBehaviour
 {
-    public void OnTriggerEnter2D(Collider2D collision)
+    [SerializeField] float horizontalMoveSpeed = 5f;
+    [SerializeField] float verticalMoveSpeed = 5f;
+
+    float maxHorizontalOffset = 10f;
+    float maxVerticalOffset = 7f;
+
+    Vector2 input;
+    Vector2 offset = Vector2.zero;
+
+    Rigidbody2D riderRigidbody;
+
+    void Awake()
+    {
+        riderRigidbody = GetComponent<Rigidbody2D>();
+        riderRigidbody.interpolation = RigidbodyInterpolation2D.Interpolate;
+    }
+
+    void OnEnable()
+    {
+        InputManager.Riding_OnMove += OnMove;
+    }
+
+    void OnDisable()
+    {
+        InputManager.Riding_OnMove -= OnMove;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Obstacle"))
         {
             RidingGame.Instance.PlayerHit();
         }
+    }
+
+    void FixedUpdate()
+    {
+        if (RidingPlayer.Instance == null)
+            return;
+
+        Vector2 cameraPosition = RidingPlayer.Instance.transform.position;
+
+        offset += Vector2.right * input.x * horizontalMoveSpeed * Time.fixedDeltaTime;
+        offset += Vector2.up * input.y * verticalMoveSpeed * Time.fixedDeltaTime;
+        
+        offset.x = Mathf.Clamp(offset.x, -maxHorizontalOffset, maxHorizontalOffset);
+        offset.y = Mathf.Clamp(offset.y, -maxVerticalOffset, maxVerticalOffset);
+
+        Vector2 targetPosition = cameraPosition + offset;
+        riderRigidbody.MovePosition(targetPosition);
+    }
+
+    void OnMove(Vector2 moveInput)
+    {
+        input = moveInput;
     }
 }
--- Assets/Scripts/Riding/RidingPlayer.cs
+++ Assets/Scripts/Riding/RidingPlayer.cs
@@ -4,18 +4,11 @@ public class RidingPlayer : MonoBehaviour
 {
     public static RidingPlayer Instance { get; private set; }
 
-    [Header("References")]
-    [SerializeField] Transform spriteTransform;
-
     [Header("Riding Settings")]
     [SerializeField] float moveSpeed = 5f;
     [SerializeField] float moveSpeedIncreaseRate = 0.05f;
-    [SerializeField] float horizontalMoveSpeed = 5f;
-    [SerializeField] float verticalMoveSpeed = 5f;
-    [SerializeField] float maxHorizontalOffset = 8f;
-    [SerializeField] float maxVerticalOffset = 4f;
 
-    Vector2 input;
+    public float MoveSpeed => moveSpeed;
 
     void Awake()
     {
@@ -25,38 +18,10 @@ public class RidingPlayer : MonoBehaviour
             Destroy(gameObject);
     }
 
-    void OnEnable()
-    {
-        InputManager.Riding_OnMove += OnMove;
-    }
-
-    void OnDisable()
-    {
-        InputManager.Riding_OnMove -= OnMove;
-    }
-
     void Update()
     {
         transform.Translate(Vector2.up * moveSpeed * Time.deltaTime);
 
-        spriteTransform.Translate(Vector2.right * input.x * horizontalMoveSpeed * Time.deltaTime);
-        spriteTransform.Translate(Vector2.up * input.y * verticalMoveSpeed * Time.deltaTime);
-
         moveSpeed += moveSpeedIncreaseRate * Time.deltaTime;
-
-        ClampPosition();
-    }
-
-    void ClampPosition()
-    {
-        Vector3 clampedPosition = spriteTransform.localPosition;
-        clampedPosition.x = Mathf.Clamp(clampedPosition.x, -maxHorizontalOffset, maxHorizontalOffset);
-        clampedPosition.y = Mathf.Clamp(clampedPosition.y, -maxVerticalOffset, maxVerticalOffset);
-        spriteTransform.localPosition = clampedPosition;
-    }
-
-    void OnMove(Vector2 moveInput)
-    {
-        input = moveInput;
     }
 }
